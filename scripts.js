1ef8f0097832ee4bad32d1e1666ddebea4f59355
Nancy El-Sharkawi <elsharkawinancy@gmail.com>
2020-09-18 00:55:38 +0300

    question 2 is ready

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const counter = document.querySelector(".counterButton");
+
+let count = 0;
+
+counter.addEventListener("click", incrementCounter);
+
+function incrementCounter() {
+  count++;
+  counter.innerHTML = count;
+  if (count === 1) {
+    document.getElementById("status").innerHTML = "جيد";
+  } else if (count > 1 && count < 6) {
+    document.getElementById("status").innerHTML = "جيد جداً";
+  } else {
+    document.getElementById("status").innerHTML = "رائع";
+  }
+}
