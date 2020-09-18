5610c278ad160fdd049365ebf703b522edd5459c
Nancy El-Sharkawi <elsharkawinancy@gmail.com>
2020-09-18 19:24:03 +0300

    cleanup + reset

--- Maybe/color-scripts.js
+++
@@ -1,9 +0,0 @@
-const colorButton = document.querySelector(".colorButton");
-const background = document.querySelector("body");
-
-const colors = ["green", "#000080", "red", "yellow"];
-
-function changeColor() {
-  let randomColor = Math.floor(Math.random() * colors.length);
-  background.style.backgroundColor = colors[randomColor];
-}
